4d9e524c77a043e602fc8178febdd20c584262f7
Filip Osoianu <filip.osoianu@sens.media>
2021-02-28 14:58:56 +0200

    update view

--- Application/app.py
+++ Application/app.py
@@ -1,9 +1,7 @@
 """
 This script runs the python_webapp_flask application using a development server.
 """
-from sklearn.linear_model import LinearRegression
-from utils import get_data, parce_data, predictt
-from flask import request, jsonify
+
 from os import environ
 from python_webapp_flask import app
 
@@ -14,29 +12,4 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
     
-    @app.route('/predict', methods=['GET'])
-    def predict():
-        data = get_data('apartmentComplexData.csv')
-        model = LinearRegression()
-        X_train, Y_train, X_test, Y_test = parce_data(data)
-
-        predict_array = []
-        request_data = request.get_json()
-        for req in request_data:
-            predict_array.append([
-                float(req["IDK1"]),
-                float(req["IDK2"]),
-                float(req["ComplexAge"]),
-                float(req["TotalRooms"]),
-                float(req["TotalBedrooms"]),
-                float(req["ComplexInhabitants"]),
-                float(req["ApartamentsNumber"]),
-                float(req["IDK3"]),
-            ])
-        predictions = predictt(model, predict_array, X_train, Y_train)
-        response = {}
-        for i in range(len(predictions)):
-            response['MedianComplexValue ' + str(i)] = predictions[i]
-        return jsonify(response)
-
     app.run(HOST, PORT)
--- Application/python_webapp_flask/views.py
+++ Application/python_webapp_flask/views.py
@@ -3,35 +3,33 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, jsonify
 from python_webapp_flask import app
+from .utils import get_data, parce_data, predictt
+from sklearn.linear_model import LinearRegression
+
 
 @app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route('/predict', methods=['GET'])
+def predict():
+    data = get_data('apartmentComplexData.csv')
+    model = LinearRegression()
+    X_train, Y_train, X_test, Y_test = parce_data(data)
+    predict_array = []
+    request_data = request.get_json()
+    for req in request_data:
+        predict_array.append([
+            float(req["IDK1"]),
+            float(req["IDK2"]),
+            float(req["ComplexAge"]),
+            float(req["TotalRooms"]),
+            float(req["TotalBedrooms"]),
+            float(req["ComplexInhabitants"]),
+            float(req["ApartamentsNumber"]),
+            float(req["IDK3"]),
+        ])
+    predictions = predictt(model, predict_array, X_train, Y_train)
+    response = {}
+    for i in range(len(predictions)):
+        response['MedianComplexValue ' + str(i)] = predictions[i]
+    return jsonify(response)
